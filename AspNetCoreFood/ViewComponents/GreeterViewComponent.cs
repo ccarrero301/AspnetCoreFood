89634b003575cf80eed3fe22650f31a568c587d7
Carlos Carrero <carlos.carrero@gmail.com>
2018-05-14 09:59:05 -0500

    Partial views and view components

---
+++ AspNetCoreFood/ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCoreFood.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreFood.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        private readonly IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetMessageOfTheDay();
+
+            return View("Default", model);
+        }
+    }
+}
